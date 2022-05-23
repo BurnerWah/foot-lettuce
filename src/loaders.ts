77b4ccd47506a466f9dcafdaec35fd0c172a2e72
Jaden Pleasants <jadenpleasants@fastmail.com>
2022-05-23 15:18:59 -0500

    Added functon factory for WEBM handlers

--- src/loaders.ts
+++ src/loaders.ts
@@ -2,50 +2,45 @@ import { videos } from './videos'
 
 const NoWebm = new Set(['TelegramBot (like TwitterBot)'])
 
+function WebmHandlerGen({ webm, mp4 }: { webm: videos; mp4: videos }) {
+  return async function (request: Request) {
+    const ua = request.headers.get('User-Agent')
+    if (ua && NoWebm.has(ua)) {
+      const video = await MY_BUCKET.get(mp4)
+      return new Response(video?.body, {
+        headers: {
+          'Content-Type': 'video/mp4',
+          'Cache-Control': 'public, max-age=2629800',
+        },
+      })
+    } else {
+      const video = await MY_BUCKET.get(webm)
+      return new Response(video?.body, {
+        headers: {
+          'Content-Type': 'video/webm',
+          'Cache-Control': 'public, max-age=2629800',
+        },
+      })
+    }
+  }
+}
+
 export const loaders: Record<string, (req: Request) => Promise<Response>> = {
   original: async () => {
     const video = await MY_BUCKET.get(videos.original)
     return new Response(video?.body, {
       headers: {
         'Content-Type': 'video/mp4',
-        'Condent-Length': '4451123',
+        'Cache-Control': 'public, max-age=2629800',
       },
     })
   },
-  game_over: async (request: Request) => {
-    const ua = request.headers.get('User-Agent')
-    if (ua && NoWebm.has(ua)) {
-      const video = await MY_BUCKET.get(videos.game_over_mp4)
-      return new Response(video?.body, {
-        headers: {
-          'Content-Type': 'video/mp4',
-        },
-      })
-    } else {
-      const video = await MY_BUCKET.get(videos.game_over)
-      return new Response(video?.body, {
-        headers: {
-          'Content-Type': 'video/webm',
-        },
-      })
-    }
-  },
-  all_clear: async (request: Request) => {
-    const ua = request.headers.get('User-Agent')
-    if (ua && NoWebm.has(ua)) {
-      const video = await MY_BUCKET.get(videos.all_clear_mp4)
-      return new Response(video?.body, {
-        headers: {
-          'Content-Type': 'video/mp4',
-        },
-      })
-    } else {
-      const video = await MY_BUCKET.get(videos.all_clear)
-      return new Response(video?.body, {
-        headers: {
-          'Content-Type': 'video/webm',
-        },
-      })
-    }
-  },
+  game_over: WebmHandlerGen({
+    webm: videos.game_over,
+    mp4: videos.game_over_mp4,
+  }),
+  all_clear: WebmHandlerGen({
+    webm: videos.all_clear,
+    mp4: videos.all_clear_mp4,
+  }),
 }
